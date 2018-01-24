45dcb47afe11c81578bddbadc65453ebac9e0a86
CharmSun <suncan0812@gmail.com>
2018-01-24 15:39:59 +0800

    build doc

--- webpack.docs.js
+++ webpack.docs.js
@@ -50,25 +50,13 @@ module.exports = {
     ]
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ],
-  devtool: 'source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'examples'),
-    host: '0.0.0.0',
-    inline: true,
-    hot: true,
-    // historyApiFallback: true,
-    proxy: {
-      '*.do': {
-        bypass: function (req, res, proxyOptions) {
-          console.log(req.url);
-          if (req.url.indexOf('.do') !== -1) {
-            req.method = 'GET';
-            return '/mock' + req.url.replace('.do', '.json');
-          }
-        }
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
       }
-    }
-  }
+    })
+  ]
 };
